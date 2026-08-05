65889978d801c571451d649d2addf7fde9d90af5
jayadeepak126-oss <jayadeepak126@gmail.com>
2026-08-05 09:48:00 -0700

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if (len(s) != len(t)):
+            return False
+        countS, countT = {}, {}
+        for char in range(len(s)):
+            countS[s[char]] = countS.get(s[char],0) + 1;
+            countT[t[char]] = 1 + countT.get(t[char],0);
+        
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True
